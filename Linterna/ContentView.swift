4d2e4198aad3dcb642ae4e1cc396b76e9a2708b7
faraday58 <jarodriguezvera@gmail.com>
2023-08-02 17:00:53 -0600

    Mis cambios de linterna

---
+++ Linterna/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Linterna
+//
+//  Created by alumno on 02/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var lamp = false
+    var body: some View {
+        VStack{
+            ZStack{
+                Color(lamp ? .black : .white)
+                Button(action: {
+                    lamp.toggle()
+                }){
+                    Text("Encender")
+                }
+                
+            }
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Linterna/LinternaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinternaApp.swift
+//  Linterna
+//
+//  Created by alumno on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LinternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
